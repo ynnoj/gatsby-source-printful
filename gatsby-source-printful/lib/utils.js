fa51cf6c8071c2dee167556ae9a8be5648dc06b3
sven <svengau@gmail.com>
2020-06-01 17:21:17 +0700

    BREAKING CHANGE: prices are no longer multiplied by 100 (#17)

--- gatsby-source-printful/lib/utils.js
+++ gatsby-source-printful/lib/utils.js
@@ -2,4 +2,4 @@ exports.base64Encode = (string) => Buffer.from(string).toString('base64')
 
 exports.parseNameForSlug = (name) => name.replace(/[\s-]+/g, '-').toLowerCase()
 
-exports.parsePriceString = (price) => Math.floor(parseInt(price) * 100)
+exports.parsePriceString = (price) => parseFloat(price)
