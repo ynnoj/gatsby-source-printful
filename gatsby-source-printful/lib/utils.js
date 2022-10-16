f02d079b838072aeaa4460b0dd01d54d7fc4afa9
Jamie Barton <jamie@notrab.dev>
2022-10-16 12:48:21 +0100

    fix: Use new Printful auth header format (#82)
    BREAKING CHANGE: Migrate Printful API token to new format. See: https://help.printful.com/hc/en-us/articles/4632388335260-What-should-I-know-about-API-key-to-API-token-migration

--- gatsby-source-printful/lib/utils.js
+++ gatsby-source-printful/lib/utils.js
@@ -1,5 +1,3 @@
-exports.base64Encode = (string) => Buffer.from(string).toString('base64')
-
 exports.parseNameForSlug = (name) => name.replace(/[\s-]+/g, '-').toLowerCase()
 
 exports.parsePriceString = (price) => parseFloat(price)
